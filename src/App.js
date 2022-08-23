bfb56a908f64cac03ef7b8a382ea08157f220341
Dawon00 <da6921@naver.com>
2022-08-24 06:43:15 +0900

    FIX - Detail path 수정

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import {BrowserRouter ,Routes, Route} from 'react-router-dom'
-import Header from './components/Home_components/Header'
-import Footer from './components/Home_components/Footer'
-import Home from './page/Home';
-import Bookmark from './page/Bookmark';
-import Detail from './page/Detail'
-import Profile from './page/Profile'
-import Search from './page/Search';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Home_components/Header";
+import Footer from "./components/Home_components/Footer";
+import Home from "./page/Home";
+import Bookmark from "./page/Bookmark";
+import Detail from "./page/Detail";
+import Profile from "./page/Profile";
+import Search from "./page/Search";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path = "/bookmark" element = {<Bookmark/>}/>
-        <Route path = "/:id" element = {<Detail/>}/>
-        <Route path = "/profile" element = {<Profile/>} />
-        <Route path = "/search" element = {<Search/>}/>
-        
-        <Route path = "/" element = {<Home/>}/>
-        <Route path = "/home/*" element = {<Home/>}/>
+        <Route path="/bookmark" element={<Bookmark />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/search" element={<Search />} />
+
+        <Route path="/" element={<Home />} />
+        <Route path="/home/*" element={<Home />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
-    
   );
 }
 
