df2dd6af1e3145d835fe131c40926ba7397b8ceb
kimwest00 <kimwest00@hufs.ac.kr>
2022-08-27 04:51:48 +0900

    FIX- Profile 입력란 style 수정

--- src/page/Profile.js
+++ src/page/Profile.js
@@ -1,30 +1,60 @@
 import { useRecoilState} from "recoil";
 import {userStoredList} from '../atom';
 import styled from "styled-components";
-
+import Footer from "../components/Home_components/Footer";
 const Wrap = styled.div`
-    overflow-x:hidden;
-    position: relative;
-    width: 95%;
-    margin: 0 auto;
-    top:80px;
-    height: 72vh;
-    bottom: 160px;
-`
+    height:100%;
 
+`
+const ProfileDiv = styled.div`
+    margin:60px;
+    min-height: 80vh;
+    outline:0;
+    border:0;
+    overflow-y:scroll;
+    overflow-y:hidden;
+`
+const ProfileText = styled.p `
+margin:20px;
+
+`
+const ProfileResult = styled.form`
+    background-color: ${(props) => props.theme.pointBackColor};
+    height: 40px;
+    line-height: 20px ;
+    border-radius:10px ;
+    padding: 10px;
+    color: grey;
+    button {
+        color: grey;
+        font-size:15px;
+        cursor:pointer;
+    }
+`
+const ProfileInput = styled.input`
+outline: none;
+    background-color: inherit;
+`
 const Profile = ()=>{
  const [userName, setUserName] = useRecoilState(userStoredList);
   console.log(userName);
   return(
     <Wrap>
-      성함을 입력해주세요 
-      <input
+      <ProfileDiv>
+        <ProfileText>
+        성함을 입력해주세요 
+        </ProfileText>
+      <ProfileResult>
+      <ProfileInput
       value ={userName}
       onChange={(e)=>{
         setUserName(e.target.value);
       }
       }
       />
+      </ProfileResult>
+      </ProfileDiv>
+      <Footer/>
     </Wrap>
     
   );
