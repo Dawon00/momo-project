3df03092c3d0c52f10066aabdcb303219348e96e
Dawon00 <da6921@naver.com>
2022-08-26 15:48:59 +0900

    FIX - detail 페이지 design 개선

--- src/atom.js
+++ src/atom.js
@@ -1,25 +1,30 @@
-import {atom} from 'recoil';
-import {localStorageEffect} from './page/Profile';
+import { atom } from "recoil";
+import { localStorageEffect } from "./page/Profile";
 import { recoilPersist } from "recoil-persist";
 
-const {persistAtom} = recoilPersist({
-    key:"toDoLocal",
-    storage:localStorage,
-})
-
+const { persistAtom } = recoilPersist({
+  key: "toDoLocal",
+  storage: localStorage,
+});
 
 export const locaCateState = atom({
-    key: "locaCateState",
-    default: ""
+  key: "locaCateState",
+  default: "",
 });
 
 export const menuCateState = atom({
-    key: "menuCateState",
-    default: ""
+  key: "menuCateState",
+  default: "",
 });
 
 export const userStoredList = atom({
-    key: "userStoredList",
-    default: "",
-    effects_UNSTABLE: [persistAtom]
+  key: "userStoredList",
+  default: "",
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const bookmarkRes = atom({
+  key: "bookmarkRes",
+  default: "",
+  effects_UNSTABLE: [persistAtom],
 });
