ef9ad7ea4848c622c8727686c9d1b3d1ef520936
kimwest00 <kimwest00@hufs.ac.kr>
2022-08-26 23:55:38 +0900

    FIX - search 미리보기 사진추가

--- src/page/Search.js
+++ src/page/Search.js
@@ -4,40 +4,55 @@ import data from "../components/api_components/Api.json"
 import CategoryBox from "../components/Home_components/CategoryBox";
 import { useMatch, useParams } from "react-router-dom";
 import { useLocation } from "react-router";
+import Footer from "../components/Home_components/Footer";
 
 const Wrap = styled.div`
-    margin-top:20px;
+    height:100%;
+
 `
-const Category = styled.div`
-    display:grid;
-    grid-template-columns:1fr 1fr ;
+const SearchDiv = styled.div`
+    margin:60px;
+    min-height: 80vh;
     width: 100%;
     overflow-y:scroll;
     overflow-y:hidden;
 `
+const SearchText = styled.p `
+
+`
+const SearchResult = styled.div`
+    display:grid;
+    grid-template-columns:1fr 1fr ;
+`
 const Search=()=>{
     const location = useLocation();
-    const {cate} = useParams();
     const userInput = new URLSearchParams(location.search).get("keyword");
     const filtered = data.apiList.filter((itemList) => {
       return itemList.name.toUpperCase().includes(userInput.toUpperCase());
     });
-    const match = useMatch(`/${cate}`);
     return (
     <Wrap>
-        <Category>
-            {filtered.map((itemList) => 
-            <div key = {itemList.key}isActive = {match !== null}>
-                <CategoryBox 
-                index = {itemList.key}
-                name = {itemList.name}
-                bCategory = {itemList.bCategory}
-                bLocation = {itemList.bLocation}
-                hastag = {itemList.hashtag}
-                />
-       </div>
-       )}
-        </Category>  
+        <SearchDiv>
+            <SearchText>
+                "{userInput}"에 관한 검색결과 {userInput.length}건
+            </SearchText>
+            <SearchResult>
+               {filtered.map((itemList) => 
+                <div key = {itemList.key}>
+                    <CategoryBox 
+                    index = {itemList.key}
+                    name = {itemList.name}
+                    bCategory = {itemList.bCategory}
+                    bLocation = {itemList.bLocation}
+                    hastag = {itemList.hashtag}
+                    url = {itemList.photoUrl}
+                    />
+                </div> )}
+        </SearchResult>
+            
+       
+        </SearchDiv>
+        <Footer/>
     </Wrap>
       
     );
